a60a7ef7eaa3a176d3a9110c3b809081d4e99abb
pvvhoang <pvvhoang@gmail.com>
2026-04-30 09:45:37 +1000

    fix issues of h5ad files for converting to cxg

--- my_scripts/convert_h5ad_to_cxg.py
+++ my_scripts/convert_h5ad_to_cxg.py
@@ -4,13 +4,39 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import scanpy as sc
+import h5py
+from cellxgene_schema.utils import read_h5ad
+
 from backend.layers.processing.h5ad_data_file import H5ADDataFile
 
 # Set input information
 input_file="my_data/test2.h5ad"
+tmp_file="my_data/tmp.h5ad"
 output_folder="my_data/test2.cxg"
-sparse_threshold=10 # for 10%, percentage of non-zero values in the matrix above which the matrix will be considered dense and below which it will be considered sparse
+sparse_threshold=25.0 # for 25%, percentage of non-zero values in the matrix above which the matrix will be considered dense and below which it will be considered sparse
 dataset_version_id="dataset_version_id"
+schema_version = "7.0.0"
+dataset_title = "dataset_title"
+need_format_correction = False
+
+def correct_h5ad_format(input_file=input_file, converted_file=tmp_file, schema_version=schema_version, dataset_title=dataset_title):
+
+    # Fix the issue of wrong format of obs
+    adata = sc.read_h5ad(input_file)
+    adata.write_h5ad(converted_file)
+    
+    # Fix the issue of missing required metadata fields in uns
+    # https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/7.0.0/schema.md
+    adata = sc.read_h5ad(converted_file)
+    adata.uns['schema_version'] = schema_version
+    adata.uns['title'] = dataset_title
+    adata.write_h5ad(converted_file)
+
+    return converted_file
+
+if need_format_correction:
+    input_file = correct_h5ad_format(input_file=input_file, converted_file=tmp_file, schema_version=schema_version, dataset_title=dataset_title)
 
 # Initialize with H5AD file
 h5ad_file = H5ADDataFile(
