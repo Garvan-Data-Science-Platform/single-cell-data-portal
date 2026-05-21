70987a6c2ea49f120517d0848510d1aa849d3bfe
pvvhoang <pvvhoang@gmail.com>
2026-05-21 16:49:45 +1000

    add USE_CANONICAL env var

--- backend/portal/api/explorer_url.py
+++ backend/portal/api/explorer_url.py
@@ -10,5 +10,8 @@ def generate(dataset: DatasetVersion, use_canonical=True):
     will use dataset version ID if use_canonical is set to False.
     """
     frontend_url = os.getenv("FRONTEND_URL", "")
+    use_canonical_var = os.getenv("USE_CANONICAL")
+    if use_canonical_var is not None:
+        use_canonical = use_canonical_var.lower() not in ("false", "0", "")
     dataset_id = dataset.dataset_id if use_canonical else dataset.version_id
     return f"{frontend_url}/e/{dataset_id}.cxg/"
