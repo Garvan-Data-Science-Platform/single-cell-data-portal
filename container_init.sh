210bd8eb1686808b16bcfd4d719ca7ce13b9918e
pvvhoang <pvvhoang@gmail.com>
2026-08-04 10:26:39 +1000

    use http for backend

--- container_init.sh
+++ container_init.sh
@@ -55,7 +55,8 @@ fi
 
 if [ "${DEPLOYMENT_STAGE}" == "test" ]; then
   # Use locally-generated cert for HTTPS in containerized local environment; deployed envs use ELB
-  HTTPS_CERT_AND_KEY="--certfile /tmp/pkcs12/server.crt --keyfile /tmp/pkcs12/server.key"
+  # HTTPS_CERT_AND_KEY="--certfile /tmp/pkcs12/server.crt --keyfile /tmp/pkcs12/server.key"
+  HTTPS_CERT_AND_KEY=""
 fi
 
 # TODO: Fix problem with invoking gunicorn under ddtrace-run. Also make sure when
